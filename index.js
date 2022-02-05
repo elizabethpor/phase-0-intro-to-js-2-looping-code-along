b0cb0f6789e9694e3b5a6c66740e254334d59381
Elizabeth <elizabeth.por@gmail.com>
2022-02-04 18:44:52 -0600

    commit tree 812458e21e54a1810075e7cab68558252256663f

--- index.js
+++ index.js
@@ -1,58 +1,46 @@
-// Code your solutions in this file
-//for (let age = 30; age < 40; age++) {
-//    console.log(`I'm ${age} years old. Happy birthday to me!`);
-//}
-
-//const gifts = ["teddy bear", "drone", "doll"];
-//function wrapGifts(gifts) {
-//    for (let i = 0; i < gifts.length; i++) {
-//        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-//    }
-//    return gifts;
-//}
-//wrapGifts(gifts);
-
-//const arrayNames = ["Charlie", "Samip", "Ali"];
-//const eventName = "birthday";
-//function writeCards(arrayNames) {
- //   for (let i = 0; i < arrayNames.length; i++) {
-//        console.log(`Thank you, ${arrayNames[i]}, for the wonderful surprise gift!`);
-//    }
-//    return writeCards;
-//}
-//writeCards[arrayNames];
-//writeCards(["Charlie", "Samip", "Ali"], "birthday");
-
-const arrayNames = ["Charlie", "Samip", "Ali"];
-const eventName = "birthday";
-function writeCards(arrayNames, eventName) {
-    const arrayMessages = [];
-    for (let i = 0; i < arrayNames.length; i++) {
-        let string = `Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`;
-        console.log(string);
-        arrayMessages.push(string);
-    }
-    return arrayMessages;
+// // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
 }
-writeCards(arrayNames, eventName);
 
+const gifts = ["teddy bear", "drone", "doll"];
 
-//const integer = 10;
-//function countDown(integer){
-//    let counter = integer;
-//    while (counter >= 0);
-//    console.log(counter--)
-//}
-//countDown(integer);
+function wrapGifts(gifts) {
+for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+    return gifts;
+}
+wrapGifts(gifts);
 
+// with a for loop
+function writeCards(arrayOfNames, eventName) {
+    const arrayOfMessages = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        let message = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`;
+        arrayOfMessages.push(message);
+    }
+    return arrayOfMessages;
+}
+writeCards(arrayOfNames, eventName);
+
+// with a while loop
+// function writeCards(arrayOfNames, eventName) {
+//     const arrayOfMessages = [];
+//     let i = 0;
+//     while (i < arrayOfNames.length) {
+//         let message = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`;
+//         arrayOfMessages.push(message);
+//         i++;
+//     }
+//     return arrayOfMessages;
+// }
+// writeCards(arrayOfNames, eventName);
 
 function countDown(integer) {
-//   console.log("I AM HERE: ",integer);
-   let i = integer;
+    let i = integer;
     while (i > 0) {
         i = integer--;
         console.log(i);
-//        return i;  
     }
 }
-//countDown(integer);
